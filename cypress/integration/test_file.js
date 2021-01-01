d7dc523deefe959aa0f55762f408212b156e81eb
achoo486 <saliraza486@gmail.com>
2021-01-01 21:46:47 +0500

    Ex.11.9

--- cypress/integration/test_file.js
+++ cypress/integration/test_file.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef, no-console*/
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    console.log('test started running');
+    console.log('test started running')
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
