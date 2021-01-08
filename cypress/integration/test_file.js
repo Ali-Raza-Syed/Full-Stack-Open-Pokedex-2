48cf48e73a7f40cdf65c2be209d20ea97a76dfce
achoo486 <saliraza486@gmail.com>
2021-01-08 21:50:30 +0500

    Ex.11.9 completed

--- cypress/integration/test_file.js
+++ cypress/integration/test_file.js
@@ -6,4 +6,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('page detail can be opened', function() {
+    console.log('test started running')
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
